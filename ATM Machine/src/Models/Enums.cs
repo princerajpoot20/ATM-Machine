b90d1d82d5959dd04fcc9dc53e900c37b358b6bb
princerajpoot20 <prince.rajpoot.20@gmail.com>
2024-09-03 21:09:04 +0530

    Add project files.

---
+++ ATM Machine/src/Models/Enums.cs
@@ -0,0 +1,15 @@
+﻿namespace ATM_Machine.src.Models;
+
+public enum ATMState
+{
+    InService,
+    OutOfService
+}
+
+public enum CurrencyDenomination
+{
+    Fifty =50,
+    OneHundred= 100,
+    TwoHundred= 200,
+    FiveHundred=500
+}
