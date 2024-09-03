24ad2b59598b410ab0f1bbe35a52e64405553d25
princerajpoot20 <prince.rajpoot.20@gmail.com>
2024-09-04 02:28:33 +0530

    Improvement in cash dispenser and implemented UI of the project

--- ATM Machine/src/Services/AccountService.cs
+++ ATM Machine/src/Services/AccountService.cs
@@ -1,6 +1,44 @@
-﻿namespace ATM_Machine.src.Services;
+﻿using ATM_Machine.HardwareImplementation;
+using ATM_Machine.src.data;
+using ATM_Machine.src.Models;
 
-public class AccountService
+namespace ATM_Machine.src.Services
 {
-    
+
+    public class AccountService
+    {
+        private CashDispenser _cashDispenser= new CashDispenser();
+
+
+        public int CheckBalance(Account account)
+        {
+            return account.Balance;
+        }
+
+        public bool Withdraw(Account account, int amount)
+        {
+            if (account.Balance < amount)
+            {
+                Console.WriteLine("Insufficient balance");
+                return false;
+            }
+
+
+            if (_cashDispenser.DispenseCash(amount))
+            {
+                account.Balance -= amount;
+                CardAccountDetails.UpdateAccount(account);
+                Console.WriteLine("Withdrawn {0}. Remaining Balance: {1}", amount, account.Balance);
+                return true;
+            }
+
+
+            return false;
+        }
+
+        public void Deposit(Account account, int depositAmount)
+        {
+            Console.WriteLine("Need to implement deposit");
+        }
+    }
 }
